605c57b49ccfd127f77addf53a47daae0bf72081
Kushagra <kushagrapansari@gmail.com>
2025-09-09 11:59:42 +0530

    added the basic server startup logic

--- src/db/index.js
+++ src/db/index.js
@@ -5,7 +5,7 @@ import { DB_NAME } from '../constants.js';
 const connectDB= async()=>{
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-        console.log(`\n mongodb connected !!! DB HOST: ${connectionInstance.connection.host}` )
+        console.log(`\nmongodb connected !!! DB HOST: ${connectionInstance.connection.host}` )
     } catch (error) {
         console.log("mongodb connection error: ", error);
         process.exit(1);
--- src/index.js
+++ src/index.js
@@ -2,11 +2,32 @@
 import dotenv from 'dotenv';
 import connectDB from './db/index.js';
 
+import {app} from './app.js';
+
 dotenv.config({
     path:'./.env'
 });
 
-connectDB();
+
+const PORT = process.env.PORT || 8000;
+
+connectDB()
+
+.then(()=>{
+    app.on("error",(err)=>{
+        console.log("server error",err);
+        process.exit(1);
+    })
+
+    app.listen(PORT, ()=>{
+        console.log(`server is running on port: ${PORT}`)
+    })
+})
+
+.catch((err)=>{
+    console.log("mongodb connection failed : ", err);
+    process.exit(1);
+})
 
 
 
