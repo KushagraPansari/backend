a7c2608c4c9927d0431ca6621c5228e7d42af330
Kushagra <kushagrapansari@gmail.com>
2025-09-10 13:31:36 +0530

    created the basic utility files

--- src/app.js
+++ src/app.js
@@ -1,7 +1,17 @@
 import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials:true
+}));
 
+app.use(express.json({limit:"16kb"})); //Prevents someone from sending a huge JSON payload that could crash your server (DoS protection).
+app.use(express.urlencoded({extended:true,limit:"16kb"}));//Parses form data (from HTML forms like <form method="POST">
+app.use(express.static("public"))//Serves static files (like images, CSS, JS, PDFs, etc.) directly from the public folder.
+app.use(cookieParser())
 
 export {app};
